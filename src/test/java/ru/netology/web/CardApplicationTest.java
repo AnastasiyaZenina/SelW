75bdbe39142fe3400680634c3860dfd4d4b0005d
Anastasiya Zenina <stasya.soloveva.2014@mail.ru>
2022-05-08 17:30:02 +0300

    Validation check

--- src/test/java/ru/netology/web/CardApplicationTest.java
+++ src/test/java/ru/netology/web/CardApplicationTest.java
@@ -17,4 +17,34 @@ public class CardApplicationTest {
         $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
+
+    @Test
+    void nameFieldEmpty() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("");
+        $("[type='tel']").setValue("+79301003496");
+        $("[data-test-id='agreement']").click();
+        $("[type='button']").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void phoneNumberIncorrect() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("Анна Петрова");
+        $("[type='tel']").setValue("+7930100");
+        $("[data-test-id='agreement']").click();
+        $("[type='button']").click();
+        $("[data-test-id='phone'].input_invalid").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void formWithoutAgreement() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("Анна Петрова");
+        $("[type='tel']").setValue("+79301003496");
+        $("[data-test-id='agreement']");
+        $("[type='button']").click();
+        $("[data-test-id='agreement'].input_invalid .checkbox__text").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй "));
+    }
 }

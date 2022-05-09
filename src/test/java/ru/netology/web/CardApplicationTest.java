b08c74982823f49e21c26ffad47af38913e7fe3a
Anastasiya Zenina <stasya.soloveva.2014@mail.ru>
2022-05-09 19:57:52 +0300

    finalization of tests

--- src/test/java/ru/netology/web/CardApplicationTest.java
+++ src/test/java/ru/netology/web/CardApplicationTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,6 +8,12 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void cardOrderRequest() {
         open("http://localhost:9999");
@@ -29,6 +36,27 @@ public class CardApplicationTest {
     }
 
     @Test
+    void invalidename() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("Anna Ivanova");
+        $("[type='tel']").setValue("+79301003496");
+        $("[data-test-id='agreement']").click();
+        $("[type='button']").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void doubleSurname() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("Анна Иванова-Петрова");
+        $("[type='tel']").setValue("+79301003496");
+        $("[data-test-id='agreement']").click();
+        $("[type='button']").click();
+        $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+
+    @Test
     void phoneNumberIncorrect() {
         open("http://localhost:9999");
         $("[type='text']").setValue("Анна Петрова");
@@ -36,6 +64,16 @@ public class CardApplicationTest {
         $("[data-test-id='agreement']").click();
         $("[type='button']").click();
         $("[data-test-id='phone'].input_invalid").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void emptyNumberField() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("Анна Петрова");
+        $("[type='tel']").setValue("");
+        $("[data-test-id='agreement']").click();
+        $("[type='button']").click();
+        $("[data-test-id='phone'].input_invalid").shouldHave(text("Поле обязательно для заполнения"));
     }
 
     @Test
